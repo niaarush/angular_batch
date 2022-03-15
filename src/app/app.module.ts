8874bc87e3abda8271afc918c1f5e403dcba346a
golurat <golu88rathod@gmail.com>
2022-03-15 08:22:48 +0530

    index compoent aaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { MessageComponent } from './message/message.component';
 import { Message2Component } from './message2/message2.component';
 import { MoviesComponent } from './movies/movies.component';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { MoviesComponent } from './movies/movies.component';
     MessageComponent,
     Message2Component,
     MoviesComponent,
+    IndexComponent,
   ],
   imports: [
     BrowserModule,
